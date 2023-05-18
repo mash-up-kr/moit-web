cdc28840fb4a0d2c9e1fdcbf1e252a3b551819e2
Yeongkwon Cho <mason.yk3@gmail.com>
2023-05-18 22:29:24 +0900

    release v0.1.0 (#6)
    * init: vite-react-ts 프로젝트 생성
    
    * docs: Github Issue & PR 탬플릿 추가
    
    * 기본 Github Action 설정 (#2)
    
    * docs: CODEOWNERS 설정
    
    * feat: push시 build test 실행 액션 작성
    
    * feat: labeler 액션 작성
    
    * fix: 액션 라벨 추가 (#3)
    
    * feat: 라벨 추가
    
    * feat: 태그 추가 (docs, chore, test, style)
    
    * feat: eslint, prettier 설정 & README 초안 작성 (#4)
    
    * chore: README 초안 작성
    
    * chore: moduleResolution 설정 변경
    
    * chore: tsconfig 설정 변경
    
    * feat: eslint 디펜던시 추가
    
    * feat: eslint 설정 추가
    
    * feat: prettier 설정 추가
    
    * style: 기존 소스코드 lint 적용
    
    * feat: path alias 설정
    
    * style: 기존 소스코드 path alias 적용
    
    * fix: tsconfig 수정
    
    * feat: yarn-berry 적용 (#5)
    
    * feat: yarn-berry 적용
    
    * feat: vscode에서 zip file을 로드하기 위한 sdk 추가
    
    * feat: 패키지 추가 시 @types 종속성  자동등록해주는  플러그인 추가
    
    * chore: ESLint, Prettier 패키지 파일 추가
    
    * chore: vite 캐시 저장경로 변경
    
    * feat: prettier 적용되도록 sdk 추가
    
    * fix: github action 빌드테스트 명령어 수정
    
    * chore: import/order rule 설정 추가
    
    ---------
    
    Co-authored-by: otterji <youngsdg1@gmail.com>
    
    ---------
    
    Co-authored-by: 원진 <fsdjin159@gmail.com>
    Co-authored-by: 노영지 <youngsdg1@gmail.com>

---
+++ .eslintrc.cjs
@@ -0,0 +1,87 @@
+module.exports = {
+  env: {
+    "browser": true,
+    "es6": true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb-typescript',
+    "plugin:react/recommended",
+    "prettier",
+    'plugin:import/typescript',
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        directory: './src',
+      },
+    },
+    'import/parsers': { '@typescript-eslint/parser': ['.ts', '.tsx'] },
+    'import/external-module-folders': ['.yarn']
+  },
+  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['.eslintrc.cjs', 'vite.config.ts'],
+  parserOptions: {
+    ecmaFeatures: {
+      "jsx": true
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json"
+  },
+  plugins: ['react-refresh', "react", "react-hooks", "prettier", "@typescript-eslint", "import"],
+  rules: {
+    "import/no-unresolved": "error",
+    "react/react-in-jsx-scope": "off",
+    "import/no-commonjs": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/jsx-filename-extension": "off",
+    "react/function-component-definition": "off",
+    'react-refresh/only-export-components': 'warn',
+    "react/jsx-props-no-spreading": "off",
+    "import/order": [
+      "error",
+      {
+        "groups": [
+          "type",
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "unknown"
+        ],
+        "pathGroups": [
+          {
+            "pattern": "react*",
+            "group": "external",
+            "position": "before"
+          },
+          {
+            "pattern": "@hooks/*",
+            "group": "internal",
+            "position": "after"
+          },
+          {
+            "pattern": "@pages/*",
+            "group": "internal",
+            "position": "after"
+          },
+          {
+            "pattern": "@components/*",
+            "group": "internal",
+            "position": "after"
+          },
+        ],
+        "pathGroupsExcludedImportTypes": ["@tanstack*"],
+        "alphabetize": {
+          "order": "asc"
+        }
+      }
+    ]
+  },
+}
