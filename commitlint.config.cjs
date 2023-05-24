b9406338541209c0444f147c1a16e17979fd1e17
wonjin-dev <fsdjin159@gmail.com>
2023-05-25 00:15:08 +0900

    ✨ feat(env): commit lint 스코프 룰 추가

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,5 +1,9 @@
 const Configuration = {
   extends: ['gitmoji'],
+  rules: {
+    'type-case': [2, 'always', 'lower-case'],
+    "scope-empty": [2, "never"],
+  }
 }
 
 module.exports = Configuration
