01b500745ad0d85d896ee808884129c46f5b5ef7
wonjin-dev <fsdjin159@gmail.com>
2023-05-29 17:24:52 +0900

    🎨 style(env): iOS safe area 스타일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <></>;
+  return <>Hello, MO IT</>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
-import { Global } from '@emotion/react';
+import { Global, css } from '@emotion/react';
 import normalize from 'emotion-normalize';
 import ReactDOM from 'react-dom/client';
 import App from 'App';
@@ -8,7 +8,20 @@ import App from 'App';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <Global styles={normalize} />
+      <Global
+        styles={css`
+          ${normalize}
+
+          body {
+            @media only screen and (-webkit-device-pixel-ratio: 3) {
+              padding-top: env(safe-area-inset-top);
+              padding-bottom: env(safe-area-inset-bottom);
+              padding-left: env(safe-area-inset-left);
+              padding-right: env(safe-area-inset-right);
+            }
+          }
+        `}
+      />
       <App />
     </ChakraProvider>
   </React.StrictMode>,
