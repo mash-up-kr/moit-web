955a9b6c46abdab50aeb8106a5a0f923598784f9
wonjin-dev <fsdjin159@gmail.com>
2023-05-29 16:48:42 +0900

    ✨ feat(chakra): chakra UI 설치 및 normalize 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Global } from '@emotion/react';
+import normalize from 'emotion-normalize';
 import ReactDOM from 'react-dom/client';
 import App from 'App';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <Global styles={normalize} />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 );
