9bb5141d242af9aeceef2251f31481151c11ea36
wonjin-dev <fsdjin159@gmail.com>
2023-06-04 00:42:43 +0900

    🔨 chore: merge시 develop 브랜치가 아니면 브랜치 삭제 액션 추가

--- types/emotion.d.ts
+++ types/emotion.d.ts
@@ -5,6 +5,7 @@ import { colors } from '@styles/theme';
 declare module '@emotion/react' {
   export interface Theme {
     colors: typeof colors;
+    // TODO: 공통 typo 정의
     // fonts: typeof fonts;
   }
 }
