7a84b5607f2f8de7a58c4a9951c64c6f736718f9
wonjin-dev <fsdjin159@gmail.com>
2023-06-03 14:43:18 +0900

    🎨 style: theme 파일 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
-import { Global, css } from '@emotion/react';
+import { Global, ThemeProvider, css } from '@emotion/react';
 import normalize from 'emotion-normalize';
 import ReactDOM from 'react-dom/client';
 import App from 'App';
+import { chakraTheme, emotionTheme } from 'styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <Global
-        styles={css`
-          ${normalize}
+    <ThemeProvider theme={emotionTheme}>
+      <ChakraProvider theme={chakraTheme}>
+        <Global
+          styles={css`
+            ${normalize}
 
-          body {
-            @media only screen and (-webkit-device-pixel-ratio: 3) {
-              padding-top: env(safe-area-inset-top);
-              padding-bottom: env(safe-area-inset-bottom);
-              padding-left: env(safe-area-inset-left);
-              padding-right: env(safe-area-inset-right);
+            body {
+              @media only screen and (-webkit-device-pixel-ratio: 3) {
+                padding-top: env(safe-area-inset-top);
+                padding-bottom: env(safe-area-inset-bottom);
+                padding-left: env(safe-area-inset-left);
+                padding-right: env(safe-area-inset-right);
+              }
             }
-          }
-        `}
-      />
-      <App />
-    </ChakraProvider>
+          `}
+        />
+        <App />
+      </ChakraProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 );
