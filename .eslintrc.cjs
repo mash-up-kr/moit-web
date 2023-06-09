ebe7404f15454ad6bd4135fd31ec8aa3090de8b1
otterji <youngsdg1@gmail.com>
2023-06-09 13:42:57 +0900

    ✨ feat(storybook): 스토리북 초기 세팅

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,47 +1,34 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'airbnb-typescript',
-    'plugin:react/recommended',
-    'prettier',
-    'plugin:import/typescript',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'airbnb-typescript', 'plugin:react/recommended', 'prettier', 'plugin:import/typescript', 'plugin:storybook/recommended'],
   settings: {
     'import/resolver': {
       typescript: {
-        directory: './src',
-      },
+        directory: './src'
+      }
     },
-    'import/parsers': { '@typescript-eslint/parser': ['.ts', '.tsx'] },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
     'import/external-module-folders': ['.yarn'],
     react: {
-      version: 'detect',
-    },
+      version: 'detect'
+    }
   },
   parser: '@typescript-eslint/parser',
   ignorePatterns: ['.eslintrc.cjs', 'vite.config.ts'],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: './tsconfig.json'
   },
-  plugins: [
-    'react-refresh',
-    'react',
-    'react-hooks',
-    'prettier',
-    '@typescript-eslint',
-    'import',
-  ],
+  plugins: ['react-refresh', 'react', 'react-hooks', 'prettier', '@typescript-eslint', 'import'],
   rules: {
     'import/no-unresolved': 'error',
     'react/react-in-jsx-scope': 'off',
@@ -52,46 +39,33 @@ module.exports = {
     'react/function-component-definition': 'off',
     'react-refresh/only-export-components': 'warn',
     'react/jsx-props-no-spreading': 'off',
-    'import/order': [
-      'error',
-      {
-        groups: [
-          'type',
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-          'unknown',
-        ],
-        pathGroups: [
-          {
-            pattern: 'react*',
-            group: 'external',
-            position: 'before',
-          },
-          {
-            pattern: '@hooks/*',
-            group: 'internal',
-            position: 'after',
-          },
-          {
-            pattern: '@pages/*',
-            group: 'internal',
-            position: 'after',
-          },
-          {
-            pattern: '@components/*',
-            group: 'internal',
-            position: 'after',
-          },
-        ],
-        pathGroupsExcludedImportTypes: ['@tanstack*'],
-        alphabetize: {
-          order: 'asc',
-        },
-      },
-    ],
-  },
+    'import/order': ['error', {
+      groups: ['type', 'builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'unknown'],
+      pathGroups: [{
+        pattern: 'react*',
+        group: 'external',
+        position: 'before'
+      }, {
+        pattern: '@hooks/*',
+        group: 'internal',
+        position: 'after'
+      }, {
+        pattern: '@pages/*',
+        group: 'internal',
+        position: 'after'
+      }, {
+        pattern: '@components/*',
+        group: 'internal',
+        position: 'after'
+      }],
+      pathGroupsExcludedImportTypes: ['@tanstack*'],
+      alphabetize: {
+        order: 'asc'
+      }
+    }],
+    "import/no-extraneous-dependencies": ["error", {
+      "devDependencies": ["**/stories/**"],
+      "optionalDependencies": false
+    }]
+  }
 };
