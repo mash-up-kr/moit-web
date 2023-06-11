8d970b3ccb876b19fc093d7d6b232cac3e127bb2
ykwon <mason.yk3@gmail.com>
2023-06-11 17:50:31 +0900

    🐛 fix: routing 안되는 문제 수정

---
+++ src/screens/StudyRegisterScreen.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import { Box } from '@chakra-ui/react';
+
+const StudyRegisterScreen: FC = () => {
+  return <Box>스터디 생성</Box>;
+};
+
+export default StudyRegisterScreen;
