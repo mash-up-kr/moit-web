03034c292ced1a022d2dfbc35436aa492063433b
otterji <youngsdg1@gmail.com>
2023-06-15 19:12:02 +0900

    🐛 fix(eslint): prettier 적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,37 +1,53 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'airbnb-typescript', 'plugin:react/recommended', 'prettier', 'plugin:import/typescript', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:import/typescript',
+    'plugin:storybook/recommended',
+  ],
   settings: {
     'import/resolver': {
       typescript: {
         directory: './src',
         alias: {
-          '@components': './src/components'
-        }
-      }
+          '@components': './src/components',
+        },
+      },
     },
     'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx']
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
     'import/external-module-folders': ['.yarn'],
     react: {
-      version: 'detect'
-    }
+      version: 'detect',
+    },
   },
   parser: '@typescript-eslint/parser',
   ignorePatterns: ['.eslintrc.cjs', 'vite.config.ts'],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: './tsconfig.json',
   },
-  plugins: ['react-refresh', 'react', 'react-hooks', 'prettier', '@typescript-eslint', 'import'],
+  plugins: [
+    'react-refresh',
+    'react',
+    'react-hooks',
+    'prettier',
+    '@typescript-eslint',
+    'import',
+  ],
   rules: {
     'import/no-unresolved': 'error',
     'react/react-in-jsx-scope': 'off',
@@ -42,33 +58,53 @@ module.exports = {
     'react/function-component-definition': 'off',
     'react-refresh/only-export-components': 'warn',
     'react/jsx-props-no-spreading': 'off',
-    'import/order': ['error', {
-      groups: ['type', 'builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'unknown'],
-      pathGroups: [{
-        pattern: 'react*',
-        group: 'external',
-        position: 'before'
-      }, {
-        pattern: '@hooks/*',
-        group: 'internal',
-        position: 'after'
-      }, {
-        pattern: '@pages/*',
-        group: 'internal',
-        position: 'after'
-      }, {
-        pattern: '@components/*',
-        group: 'internal',
-        position: 'after'
-      }],
-      pathGroupsExcludedImportTypes: ['@tanstack*'],
-      alphabetize: {
-        order: 'asc'
-      }
-    }],
-    "import/no-extraneous-dependencies": ["error", {
-      "devDependencies": ["**/stories/**"],
-      "optionalDependencies": false
-    }]
-  }
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'type',
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'unknown',
+        ],
+        pathGroups: [
+          {
+            pattern: 'react*',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@hooks/*',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@pages/*',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@components/*',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['@tanstack*'],
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/stories/**'],
+        optionalDependencies: false,
+      },
+    ],
+  },
 };
