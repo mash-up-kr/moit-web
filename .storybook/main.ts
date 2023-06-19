38699f4cd805ebc39154afaa0d97c6c96b87b4b3
ykwon <mason.yk3@gmail.com>
2023-06-20 00:48:10 +0900

    ♻️ refactor: main.ts 모듈 적용방식 변경

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,25 +1,13 @@
-import path from 'path';
-
 import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    path.dirname(
-      require.resolve(path.join('@storybook/addon-links', 'package.json')),
-    ),
-    path.dirname(
-      require.resolve(path.join('@storybook/addon-essentials', 'package.json')),
-    ),
-    path.dirname(
-      require.resolve(
-        path.join('@storybook/addon-interactions', 'package.json'),
-      ),
-    ),
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: path.dirname(
-      require.resolve(path.join('@storybook/react-vite', 'package.json')),
-    ),
+    name: '@storybook/react-vite',
     options: {},
   },
   docs: {
