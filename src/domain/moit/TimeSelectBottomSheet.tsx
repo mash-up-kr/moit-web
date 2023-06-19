4b3e1738d55121be94c9f706a7ca604fd1588455
wonjin-dev <fsdjin159@gmail.com>
2023-06-20 03:39:08 +0900

    ♻️ refactor: 임시 상태 부여

--- src/domain/moit/TimeSelectBottomSheet.tsx
+++ src/domain/moit/TimeSelectBottomSheet.tsx
@@ -1,4 +1,4 @@
-import { FC, useRef } from 'react';
+import { FC, useRef, useState } from 'react';
 import styled from '@emotion/styled';
 import { ModalProps } from 'hooks/useModal';
 import { generateArray } from 'utils/generateArray';
@@ -15,31 +15,34 @@ interface Props {
 
 const TimeSelectBottomSheet: FC<Props> = ({
   modalProps,
-  currentTarget,
+  // currentTarget,
   startTime,
   endTime,
 }) => {
   const ITEM_HEIGHT = 52;
   const hourRef = useRef<HTMLUListElement>(null);
   const minRef = useRef<HTMLUListElement>(null);
+  // TODO: 임시 상태
+  const [tempHour, setTempHour] = useState(0);
+  const [tempMin, setTempMin] = useState(0);
 
-  const hour = currentTarget === 'start' ? startTime.hour : endTime.hour;
-  const min = currentTarget === 'start' ? startTime.minuete : endTime.minuete;
+  // const hour = currentTarget === 'start' ? startTime.hour : endTime.hour;
+  // const min = currentTarget === 'start' ? startTime.minuete : endTime.minuete;
 
   const handleHourScroll = () => {
     const top = hourRef.current && hourRef.current.scrollTop;
     const currentSelectedHour = top ? Math.round(top / ITEM_HEIGHT) : 0;
 
-    // TODO: set state
-    console.log(currentSelectedHour);
+    // TODO
+    setTempHour(currentSelectedHour);
   };
 
   const handleMinScroll = () => {
     const top = minRef.current && minRef.current.scrollTop;
     const currentSelectedMin = top ? Math.round(top / ITEM_HEIGHT) : 0;
 
-    // TODO: set state
-    console.log(currentSelectedMin);
+    // TODO
+    setTempMin(currentSelectedMin);
   };
 
   return (
@@ -57,7 +60,8 @@ const TimeSelectBottomSheet: FC<Props> = ({
             <Content>
               <SelectScroller
                 ref={hourRef}
-                selectedElements={hour}
+                // selectedElements={hour}
+                selectedElements={tempHour}
                 elements={generateArray(24)}
                 onScroll={handleHourScroll}
               />
@@ -65,7 +69,8 @@ const TimeSelectBottomSheet: FC<Props> = ({
             <Content>
               <SelectScroller
                 ref={minRef}
-                selectedElements={min}
+                // selectedElements={min}
+                selectedElements={tempMin}
                 elements={generateArray(59)}
                 onScroll={handleMinScroll}
               />
