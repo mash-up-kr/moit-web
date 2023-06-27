877914481faa2908fd2867cf19e6c94003f75384
wonjin-dev <fsdjin159@gmail.com>
2023-06-28 00:31:59 +0900

    🐛 fix: typo 수정

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -27,14 +27,15 @@ const globalStyle = css`
   }
 
   body {
+    font-family: 'Pretendard';
+    color: ${colors.text.general};
+
     @media only screen and (-webkit-device-pixel-ratio: 3) {
       padding-top: env(safe-area-inset-top);
       padding-bottom: env(safe-area-inset-bottom);
       padding-left: env(safe-area-inset-left);
       padding-right: env(safe-area-inset-right);
     }
-    font-family: Pretendard;
-    color: ${colors.text.general};
   }
 `;
 
