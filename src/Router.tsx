61e613110d5ed0a0fe3dd9188ac05cb62b50e710
ykwon <mason.yk3@gmail.com>
2023-07-04 00:43:22 +0900

    ♻️ refactor: studyRegisterScreen import  수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,7 @@
 import { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Example from 'Example';
-
-/** TODO: index 생략 가능하게 하는 옵션 아시는분.. */
-import StudyRegisterScreen from 'screens/StudyRegisterScreen/index';
+import StudyRegisterScreen from 'screens/StudyRegisterScreen';
 
 const Router: FC = () => {
   return (
