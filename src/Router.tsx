31ef714246ce9a613d3ac33d54f2823aad9ce53f
otterji <youngsdg1@gmail.com>
2023-07-15 16:50:08 +0900

    ✨ feat: 출석 결과 화면 UI 생성

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Example from 'Example';
 import AttendanceKeywordScreen from 'screens/AttendanceScreen/AttendanceKeywordScreen';
+import AttendanceResultScreen from 'screens/AttendanceScreen/AttendanceResultScreen';
 import StudyRegisterScreen from 'screens/StudyRegisterScreen';
 
 const Router: FC = () => {
@@ -11,6 +12,7 @@ const Router: FC = () => {
         <Route path="/" element={<Example />} />
         <Route path="/register" element={<StudyRegisterScreen />} />
         <Route path="/attendance" element={<AttendanceKeywordScreen />} />
+        <Route path="/attendanceResult" element={<AttendanceResultScreen />} />
       </Routes>
     </BrowserRouter>
   );
