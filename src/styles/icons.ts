177c929bd6ea254b3c65616f945c324d20e54165
ykwon <mason.yk3@gmail.com>
2023-07-18 22:39:29 +0900

    ♻️ refactor: assets 디렉토리 위치 변경, path 추가

--- src/styles/icons.ts
+++ src/styles/icons.ts
@@ -1,2 +1,2 @@
-export { ReactComponent as ArrowLeft } from '../../assets/svgs/ArrowLeft.svg';
-export { ReactComponent as Info } from '../../assets/svgs/Info.svg';
+export { ReactComponent as ArrowLeft } from '@assets/svgs/ArrowLeft.svg';
+export { ReactComponent as Info } from '@assets/svgs/Info.svg';
