18dae8ae3fa6003226c54e4693c6c214f7615962
otterji <youngsdg1@gmail.com>
2023-07-19 17:41:32 +0900

    ♻️ refactor: avatar 컴포넌트 기본값 추가

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -3,6 +3,7 @@ import {
   Avatar as ChakraAvatar,
 } from '@chakra-ui/react';
 import * as pngType from '@styles/pngs';
+import { defaultAvatar } from '@styles/pngs';
 import theme, { AvatarKeyType } from '@styles/theme';
 
 type AvatarImage =
@@ -11,16 +12,22 @@ type AvatarImage =
   | (typeof pngType.profile)[number];
 
 interface AvatarProps extends ChakraAvatarProps {
-  type: AvatarKeyType;
-  src: AvatarImage;
+  src?: AvatarImage;
+  type?: AvatarKeyType;
 }
 
-const Avatar = ({ type, src, ...restProps }: AvatarProps) => {
+/**
+ *
+ * @param param0 src, type은 모두 optional입니다
+ * @returns 아무 값도 넘기지 않은 경우 ? 모양의 md 사이즈 Avatar 기본값이 출력됩니다
+ */
+const Avatar = ({ type = 'md', src, ...restProps }: AvatarProps) => {
+  const isEmpty = Boolean(!src);
   return (
     <ChakraAvatar
-      src={src}
+      src={src || defaultAvatar}
       css={theme.avatar[type]}
-      bg={theme.palette.gray100}
+      bg={isEmpty ? theme.palette.gray300 : theme.palette.gray100}
       border={`1px solid ${theme.palette.gray200}`}
       {...restProps}
     />
