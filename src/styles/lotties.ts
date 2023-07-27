3c7c6e1fef35acddc530d9f6e4da8851d5bdbc8a
wonjin-dev <fsdjin159@gmail.com>
2023-07-27 20:08:22 +0900

    🐛 fix: 빌드버전 lottie 미실행 오류 수정

--- src/styles/lotties.ts
+++ src/styles/lotties.ts
@@ -1,5 +1,5 @@
 export const lottiesPath = {
-  confetti: '../../assets/lotties/confetti.json',
-  fail: '../../assets/lotties/fail.json',
-  success: '../../assets/lotties/success.json',
+  confetti: '/assets/lotties/confetti.json',
+  fail: '/assets/lotties/fail.json',
+  success: '/assets/lotties/success.json',
 } as const;
